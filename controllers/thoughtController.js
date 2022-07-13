947c3470c5b86df090fa312cd2aad3ce46b52225
Tu Nguyen <tuinfor@ymail.com>
2022-07-12 22:45:03 -0400

    get thought by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -20,6 +20,36 @@ createThought(req, res) {
     .then(userData => res.json(userData))
     .catch((err) => res.status(500).json(err));
     },
+//   get Thought ById
+getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+    .then((dbThoughtData) => {
+        // if no thought is found
+        if (!dbThoughtData) {
+        res.status(404).json({ message: "No thought with this ID" });
+        return;
+        }
+        res.json(dbThoughtData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+},
+//update thought by it's id
+updateThought(req, res) {
+    Thought.findOneAndUpdate({
+        _id: req.params.id
+    }, {
+        $set: req.body
+    }, {
+        runValidators: true,
+        new: true
+    }).then((thought) => {
+        !thought ? res.status(404).json({message: 'No thought by ID'}) : res.json(thought);
+
+    }).catch((err) => res.status(500).json(err));
+},
 }
 
 module.exports = thoughtController;
