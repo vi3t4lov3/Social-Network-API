aed9100335be896778b5bb8a7eebba4c0cfbc24f
Tu Nguyen <tuinfor@ymail.com>
2022-07-12 22:29:06 -0400

    get all the user, user by id & update

---
+++ controllers/userController.js
@@ -0,0 +1,35 @@
+const req = require("express/lib/request");
+const { Thought, User } = require("../models");
+
+const userController = {
+    // get all users
+    getAllUsers(req, res) {
+        User.find().then((users) => res.json(users)).catch((err) => res.status(500).json(err));
+
+    },
+    // create user
+    createUser(req, res) {
+        User.create(req.body).then((dbUserData) => res.json(dbUserData)).catch((err) => res.status(500).json(err));
+    },
+
+    // update user by id
+    updateUser(req, res) {
+        User.findOneAndUpdate({
+            _id: req.params.id
+        }, {
+            $set: req.body
+        }, {
+            runValidators: true,
+            new: true
+        }).then((user) => {
+            !user ? res.status(404).json({ message: 'No user' }) : res.json(user);
+
+        }).catch((err) => res.status(500).json(err));
+
+
+    },
+
+   
+};
+
+module.exports = userController;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const PORT = process.env.port || 3002;
+const PORT = process.env.port || 3003;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
